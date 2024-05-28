dd0c76b948758824e7f0a3a740e15eebf5dca744
Tarun <petatarun17@gmail.com>
2024-05-28 23:54:50 +0530

    Added constants ie. id they not be changed

---
+++ src/util/Constants.java
@@ -0,0 +1,7 @@
+package util;
+
+public class Constants {
+
+	public static String UPLOAD_PATH="C:\\Users\\Admin\\Desktop\\Project\\appuploads\\";
+	public static String DOWNLOAD_PATH="C:\\Users\\Admin\\Desktop\\Project\\appdownloads\\";
+}
