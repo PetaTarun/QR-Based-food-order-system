fc7517772a4c28d7ad2c2c7374dee3c1a67887e0
Tarun <petatarun17@gmail.com>
2024-06-11 20:29:31 +0530

    Added file downloader

---
+++ src/cloudme/FileDownload.java
@@ -0,0 +1,39 @@
+package cloudme;
+
+import java.io.File;
+
+import Cloudme.CloudmeFolderNode;
+import Cloudme.CloudmeUser;
+
+public class FileDownload {
+
+	public static synchronized boolean fileDownload(String filepath)
+	{
+		try
+        {
+			CloudmeUser user = new CloudmeUser("ssathvika208","voidmain");
+			
+			CloudmeFolderNode node = user.getFolderManager().getFolderTree();
+			
+			for(CloudmeFolderNode n : node.getChildren()){
+				
+				if(n.getFolder().getName().equals("keyaggrigate"))
+				{
+					n.getFolder().getFile(new File(filepath).getName()).downloadFile(filepath);
+					
+					return true;
+				}
+			}
+			
+			user.killUser();
+	    }
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			
+			return false;
+		}
+		
+		return false;
+	}
+}
